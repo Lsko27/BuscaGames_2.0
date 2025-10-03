adbd24f284d04719587e4d8c54a84d53dcd98a26
Yuri Lesko <yurilesko27@gmail.com>
2025-10-03 17:32:59 -0300

    feat: add ForgotPasswordPage

---
+++ src/app/_components/forgot-password-form.tsx
@@ -0,0 +1,73 @@
+"use client"
+
+import { useForm } from "react-hook-form"
+import { Form, FormControl, FormField, FormItem, FormMessage } from "./ui/form"
+import { Input } from "./ui/input"
+import { zodResolver } from "@hookform/resolvers/zod"
+import z from "zod"
+import { Button } from "./ui/button"
+import Link from "next/link"
+import { ArrowLeft } from "lucide-react"
+
+const forgotPasswordSchema = z.object({
+  email: z
+    .string()
+    .trim()
+    .min(1, { message: "Email é obrigatório" })
+    .email("Email inválido"),
+})
+
+type FormData = z.infer<typeof forgotPasswordSchema>
+
+const ForgotPasswordForm = () => {
+  const form = useForm<FormData>({
+    resolver: zodResolver(forgotPasswordSchema),
+    defaultValues: { email: "" },
+  })
+
+  const onSubmit = (data: FormData) => {
+    console.log("Enviar link de recuperação para:", data.email)
+    // Aqui você chamaria sua API para enviar o email de recuperação
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  placeholder="Digite seu email"
+                  {...field}
+                  className="border-purple-700 text-white"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button
+          type="submit"
+          className="w-full bg-pink-900 hover:bg-purple-700"
+        >
+          Enviar link
+        </Button>
+
+        <div className="flex items-center justify-center text-blue-400">
+          <Button asChild variant="ghost">
+            <Link href="/login">
+              <ArrowLeft />
+              <p>Voltar para Login</p>
+            </Link>
+          </Button>
+        </div>
+      </form>
+    </Form>
+  )
+}
+
+export default ForgotPasswordForm
---
+++ src/app/forgot-password/page.tsx
@@ -0,0 +1,43 @@
+import { KeyRound } from "lucide-react"
+import { Card, CardContent } from "../_components/ui/card"
+import ForgotPasswordForm from "../_components/forgot-password-form"
+import Image from "next/image"
+
+const ForgotPasswordPage = () => {
+  return (
+    <div className="relative flex min-h-[60%] items-center justify-center">
+      {/* Background */}
+      <div className="absolute inset-0">
+        <Image
+          alt="Banner"
+          src="/freepik__expand__92115.png"
+          fill
+          className="object-cover"
+        />
+        <div className="absolute inset-0 bg-gradient-to-b from-[rgba(0,0,0,0.7)] to-[rgba(0,0,0,0.6)]" />
+      </div>
+
+      {/* Conteúdo sobreposto */}
+      <div className="relative z-10 w-[50%]">
+        <Card className="bg-gray-900">
+          <CardContent>
+            <div className="flex flex-col items-center gap-2">
+              <KeyRound size={60} className="my-3 text-purple-700" />
+              <h1 className="text-center text-3xl font-semibold text-white">
+                Esqueceu a senha?
+              </h1>
+              <p className="text-center text-white">
+                Digite seu e-mail abaixo para receber o link de redefinição.
+              </p>
+            </div>
+            <div className="mt-5 px-5">
+              <ForgotPasswordForm />
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  )
+}
+
+export default ForgotPasswordPage
