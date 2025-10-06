392e7a3204150281878345678bca072f03164e3a
Yuri Lesko <yurilesko27@gmail.com>
2025-10-05 22:26:52 -0300

    chore: changing bg color and loading icon color

--- src/app/_components/loading-screen.tsx
+++ src/app/_components/loading-screen.tsx
@@ -3,8 +3,8 @@ import { Loader2 } from "lucide-react"
 
 const LoadingScreen = () => {
   return (
-    <div className="fixed inset-0 z-[9999] flex flex-col items-center justify-center bg-black text-white">
-      <Loader2 className="mb-4 h-12 w-12 animate-spin text-green-500" />
+    <div className="fixed inset-0 z-[9999] flex flex-col items-center justify-center bg-slate-950 text-white">
+      <Loader2 className="mb-4 h-12 w-12 animate-spin text-blue-500" />
       <p className="animate-pulse text-lg font-medium tracking-wide">
         Carregando...
       </p>
