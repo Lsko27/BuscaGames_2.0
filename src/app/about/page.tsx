bb7e0307e49dcad7b01a8c469890e1d036989844
Yuri Lesko <yurilesko27@gmail.com>
2025-10-06 10:09:48 -0300

    chore: making about page responsive

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -84,27 +84,30 @@ const AboutPage = () => {
         <div className="absolute inset-0 bg-gradient-to-b from-[rgba(0,0,0,0.7)] to-[rgba(0,0,0,0.6)]" />
 
         <div className="absolute inset-0 flex flex-col items-center justify-center px-4 text-center">
-          <h2 className="mb-2 text-3xl font-bold text-white md:text-5xl">
-            Sobre a BuscaGames
-          </h2>
-          <p className="text-lg text-blue-300 md:text-2xl">
-            Conheça nossa equipe e missão de levar tecnologia de ponta a você.
-          </p>
+          <div className="max-w-4xl">
+            <h2 className="mb-2 text-3xl font-bold text-white md:text-5xl">
+              Sobre a BuscaGames
+            </h2>
+            <p className="text-lg text-blue-300 md:text-2xl">
+              Conheça nossa equipe e missão de levar tecnologia de ponta a você.
+            </p>
+          </div>
         </div>
       </div>
 
       {/* CARD SOBRE NÓS */}
-      <div className="flex w-full items-center justify-center bg-gray-900 py-14">
+      <div className="flex w-full justify-center bg-gray-900 px-4 py-14 sm:px-6">
         <AboutCard />
       </div>
 
       {/* EQUIPE */}
-      <div className="bg-slate-950 py-12">
-        <h3 className="my-2 text-center text-4xl font-bold text-white">
+      <div className="bg-slate-950 px-4 py-12 sm:px-6 lg:px-24">
+        <h3 className="text-center text-4xl font-bold text-white">
           Equipe do Projeto
         </h3>
-        <p className="mb-2 text-center text-lg text-gray-300">Quem somos</p>
-        <div className="flex items-center justify-center gap-6 p-5">
+        <p className="mb-8 text-center text-lg text-gray-300">Quem somos</p>
+
+        <div className="grid grid-cols-1 justify-items-center gap-6 sm:grid-cols-2 lg:grid-cols-4 lg:items-center lg:justify-center">
           {teamMembers.map((member) => (
             <TeamCard key={member.rm} params={member} />
           ))}
@@ -112,7 +115,7 @@ const AboutPage = () => {
       </div>
 
       {/* FORMULÁRIO DE CONTATO */}
-      <div className="bg-gray-900 p-10">
+      <div className="bg-gray-900 p-10 px-4 sm:px-6">
         <h3 className="mb-8 text-center text-4xl font-bold text-white">
           Fale Conosco
         </h3>
@@ -120,14 +123,13 @@ const AboutPage = () => {
         <div className="mx-auto w-full max-w-5xl rounded-lg bg-gray-800 p-7">
           <div className="flex flex-col items-center justify-center gap-8 lg:flex-row">
             {/* ESQUERDA */}
-            <div className="flex h-full flex-col justify-between text-center lg:w-1/2 lg:text-left">
+            <div className="flex w-full flex-col justify-between text-center lg:w-1/2 lg:text-left">
               <h4 className="mb-5 text-3xl font-semibold text-indigo-300">
                 Entre em contato
               </h4>
               <p className="mb-3 text-gray-300">
                 Tem alguma dúvida ou sugestão? Estamos aqui para ajudar!
               </p>
-
               <div className="flex flex-col gap-3 text-xl text-white">
                 <FooterContact
                   icon={<Mail />}
@@ -142,7 +144,7 @@ const AboutPage = () => {
             </div>
 
             {/* DIREITA */}
-            <div className="rounded-lg bg-gray-900 p-8 lg:w-1/2">
+            <div className="w-full rounded-lg bg-gray-900 p-8 lg:w-1/2">
               <ContactAboutForm />
             </div>
           </div>
