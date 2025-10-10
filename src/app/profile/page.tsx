f52c9438b741a227d964754e31347b123caac08e
Caio Cesar <caiocpoliveira163@gmail.com>
2025-10-10 18:33:59 -0300

    fix: error in components import

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -3,11 +3,11 @@
 import { useState } from "react"
 import RequireAuth from "@/components/require-auth"
 import ProfileHeader from "@/components/profile-header"
-import OverviewTab from "@/components/overview-tab"
-import LibraryTab from "@/components/library-tab"
-import FavoriteTab from "@/components/favorite-tab"
+import OverviewTab from "./_components/overview-tab"
+import LibraryTab from "./_components/library-tab"
+import FavoriteTab from "./_components/favorite-tab"
 import { Button } from "@/components/ui/button"
-import AchievementsTab from "@/components/achievements-tab"
+import AchievementsTab from "./_components/achievements-tab"
 
 const tabs = [
   { key: "overview", label: "Visão Geral" },
